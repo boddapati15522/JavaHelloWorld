f8811bf3220b6bafffb24fedec455a6c23e31755
boddapati15522 <lboddapati6@gmail.com>
2019-09-24 15:29:34 -0400

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
    public static void main(String[] args) {
        // prints "hello, world" in the terminal window.
        System.out.println("hello, world");
+       System.exit( 0 ); //success
     }
 }
 
